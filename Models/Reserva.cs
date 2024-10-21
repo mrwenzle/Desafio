d668734b0d791292ff415150b6797e069d325c41
Gabriel_Santos <mrwenzle10@gmail.com>
2024-10-21 14:05:18 -0300

    incial

---
+++ Models/Reserva.cs
@@ -0,0 +1,52 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            int capacidade = 3;
+            if (capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Capacidade menor que o número de hóspedes");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            int quantidade = Hospedes.Count;
+            return quantidade;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valorTotal = DiasReservados * Suite.ValorDiaria;
+            decimal desconto = DiasReservados >= 10 ? valorTotal * 0.1M : 0;
+            decimal valorFinal = valorTotal - desconto;
+
+            return valorFinal;
+        }
+    }
+}
